a8ecb5157dfbfe7f61e313ac56bd6e86749cac43
Ryan Doherty <tech@conical.org>
2024-09-04 22:06:16 -0400

    Add question lookup by wsf request

--- WSFPlugin/src/main/java/org/eupathdb/websvccommon/wsfplugin/PluginUtilities.java
+++ WSFPlugin/src/main/java/org/eupathdb/websvccommon/wsfplugin/PluginUtilities.java
@@ -4,17 +4,21 @@ import org.gusdb.fgputil.runtime.GusHome;
 import org.gusdb.fgputil.runtime.InstanceManager;
 import org.gusdb.wdk.model.Utilities;
 import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.question.Question;
 import org.gusdb.wdk.model.record.RecordClass;
 import org.gusdb.wsf.plugin.PluginModelException;
 import org.gusdb.wsf.plugin.PluginRequest;
 
 public class PluginUtilities {
 
-  public static RecordClass getRecordClass(PluginRequest request) throws PluginModelException {
+  public static Question getContextQuestion(PluginRequest request) throws PluginModelException {
     String questionFullName = request.getContext().get(Utilities.QUERY_CTX_QUESTION);
     return getWdkModel(request.getProjectId()).getQuestionByFullName(questionFullName)
-      .map(question -> question.getRecordClass())
       .orElseThrow(() -> new PluginModelException("Could not find context question: " + questionFullName));
+  }
+
+  public static RecordClass getRecordClass(PluginRequest request) throws PluginModelException {
+    return getContextQuestion(request).getRecordClass();
   }
 
   public static WdkModel getWdkModel(PluginRequest request) {
